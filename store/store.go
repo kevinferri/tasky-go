c3d81f0063ba5a00fc0c1a160e93e43f9b900981
kevinferri <kevin@puzzle.io>
2023-10-03 21:40:05 -0400

    Clean up

--- store/store.go
+++ store/store.go
@@ -11,13 +11,3 @@ func InitStore(sqlDb *sql.DB) {
 	db = sqlDb
 	log.Println("✅ Store")
 }
-
-func idk() {
-	var snippet Snippet
-
-	query := `select * from snippets WHERE id=$1;`
-	row := db.QueryRow(query, id)
-	err := row.Scan(&snippet.ID, &snippet.Title)
-
-	return snippet, err
-}
